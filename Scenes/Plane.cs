493cabb4f137bdc250069442e82928cb63d27159
shanfl <shanfulin@baiyyy.com.cn>
2025-12-17 09:48:38 +0800

    animationplayer

--- Scenes/Plane.cs
+++ Scenes/Plane.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Plane : CharacterBody2D
 {
+	[Export] public float Grivaty = 9.8f;
+	[Export] public float Power = -450.0f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +13,20 @@ public partial class Plane : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		velocity.Y += Grivaty;
+
+		if (Input.IsActionJustPressed("fly"))
+		{
+			velocity.Y = Power;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
